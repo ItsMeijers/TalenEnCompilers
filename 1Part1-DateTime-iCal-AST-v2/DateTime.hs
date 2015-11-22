7c362207ede0ed7ed9c98ec1d2c95b28870258c7
ThomasMeijers <thomasmeijers@live.nl>
2015-11-22 22:47:37 +0100

    rest of exercices bug in assigment 1 with length too long

--- 1Part1-DateTime-iCal-AST-v2/DateTime.hs
+++ 1Part1-DateTime-iCal-AST-v2/DateTime.hs
@@ -2,6 +2,7 @@
 
 import ParseLib.Abstract
 import Data.List
+import Data.Time.Calendar (gregorianMonthLength)
 
 -- Starting Framework
 
@@ -97,16 +98,31 @@ run p = fmap fst . find (null . snd) . parse p
 -- Exercise 3
 printDateTime :: DateTime -> String
 printDateTime DateTime {..} = printDate date ++ "T" ++ printTime time ++ ['Z' | utc]
-  where printDate Date {..} = show (unYear year) ++ show (unMonth month) ++ show (unDay day)
-        printTime Time {..} = show (unHour hour) ++ show (unMinute minute) ++ show (unSecond second)
+  where printDate Date {..} = show' (unYear year)   4 ++
+                              show' (unMonth month) 2 ++
+                              show' (unDay day) 2
+        printTime Time {..} = show' (unHour hour)     2 ++
+                              show' (unMinute minute) 2 ++
+                              show' (unSecond second) 2
+        show' x             = fixLenght (show x)
+        fixLenght xs n
+          | length' >= n    = xs
+          | otherwise       = replicate (n - length') '0' ++ xs
+              where length' = length xs
 
 -- Exercise 4
 parsePrint s = fmap printDateTime $ run parseDateTime s
 
 -- Exercise 5
 checkDateTime :: DateTime -> Bool
-checkDateTime = undefined
-
+checkDateTime DateTime {..} = checkDate date && checkTime time
+  where checkDate Date{..} = checkMonth && checkDay
+          where checkMonth = unMonth month >= 1 && unMonth month <= 12
+                checkDay    = unDay day <= gregorianMonthLength (toInteger (unYear year)) (unMonth month) && unDay day >= 1
+        checkTime Time{..} = checkHour && checkMinute && checkSecond
+          where checkHour = unHour hour >= 0 && unHour hour <= 23
+                checkMinute = unMinute minute >= 0 && unMinute minute <= 59
+                checkSecond = unSecond second >= 0 && unSecond second <= 59
 
 
 -- Exercise 6
