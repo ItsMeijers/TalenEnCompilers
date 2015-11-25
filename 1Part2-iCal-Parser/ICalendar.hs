95b9aa6a25ec2452814a774f27711838013b59b9
ThomasMeijers <thomasmeijers@live.nl>
2015-11-25 20:45:30 +0100

    Create some parser combinators for assigment 2. TODO: introducte tokens!

--- 1Part2-iCal-Parser/ICalendar.hs
+++ 1Part2-iCal-Parser/ICalendar.hs
@@ -3,7 +3,8 @@ module ICalendar where
 import ParseLib.Abstract
 import Data.Maybe
 import Text.PrettyPrint
-
+import Data.Char
+import System.IO
 
 data DateTime = DateTime { date :: Date
                          , time :: Time
@@ -42,6 +43,51 @@ data VEvent = VEvent { dtStamp     :: DateTime
                      , location    :: Maybe String }
     deriving Eq
 
+-- Parser combinator for DateTime, gets created by combining parseDate, parseTime and parseUtc
+-- Finally DateTime gets contstructed by the parser using the <$> parser combinator
+parseDateTime :: Parser Char DateTime
+parseDateTime = DateTime <$> parseDate <*> parseTime <*> parseUtc
+
+-- Parser combinator for parsing the UTC value, results in Boolean wether the Z is present or absent
+parseUtc :: Parser Char Bool
+parseUtc = succeed True
+           <* symbol 'Z' -- Symbol Z can be ignored and the True bool will be used
+           <|> succeed False -- Or when Z is absent the False bool will be used
+
+-- Function to create a Parser combinbator for Date
+-- Extracted as seperate function since parsing an individual date might be usefull
+parseDate :: Parser Char Date
+parseDate = Date <$> parseYear <*> parseMonth <*> parseDay
+  where
+     parseYear  = Year  <$> parseDigits 4
+     parseMonth = Month <$> parseTwoDigits
+     parseDay   = Day   <$> parseTwoDigits
+
+-- Creates Parser combinator for Time
+-- Extracted as seperate function since parsing an individual Time might be usefull
+parseTime :: Parser Char Time
+parseTime = Time
+            <$> (symbol 'T' *> parseHour) -- Symbol T has to be parsed but the result can be ignored
+            <*> parseMinute
+            <*> parseSecond
+    where
+       parseHour   = Hour   <$> parseTwoDigits
+       parseMinute = Minute <$> parseTwoDigits
+       parseSecond = Second <$> parseTwoDigits
+
+-- Helper function for creating a Int parser where 2 digits are present
+-- Gets used enought for abstracting as individual function
+parseTwoDigits :: Parser Char Int
+parseTwoDigits = parseDigits 2
+
+-- Function for creating a Parser that parses n amount of digits
+-- By using sequence a Parser combinator gets created from n digit parser combinators
+-- [Char] gets conferted to Int using the digitsToNumber function
+parseDigits :: Int -> Parser Char Int
+parseDigits n = digitsToNumber <$> (ParseLib.Abstract.sequence (replicate n digit))
+  where
+     digitsToNumber = foldl addDigit 0
+     addDigit acc c = 10 * acc + digitToInt c
 
 run :: Parser a b -> [a] -> Maybe b
 run p s = listToMaybe [p | (p, []) <- parse p s]
@@ -56,14 +102,57 @@ main = do Just cal <- readCalendar "examples/rooster_infotc.ics"
 
 -- Exercise 1
 parseCalendar :: Parser Char Calendar
-parseCalendar = undefined
+parseCalendar = Calendar <$>
+                parseProdId <*>
+                many parseVEvent
+                --parseBeginVCal *> parseVersion   *>
+                -- parseProdId    <*>
+                -- many parseVEvent <*>
+                -- parseEndVCal
+
+parseVEvent :: Parser Char VEvent
+parseVEvent = undefined
+
+parseBeginVCal :: Parser Char String
+parseBeginVCal = parseTokenColonToken "BEGIN" "VCALENDAR"
+
+parseEndVCal :: Parser Char String
+parseEndVCal = parseTokenColonToken "END" "VCALENDAR"
+
+parseTokenColonToken :: String -> String -> Parser Char String
+parseTokenColonToken t t' = token t *> pColon *> token t'
+
+pColon :: Parser Char Char
+pColon = symbol ':'
 
 
+-- | Creates a parser combinator based on a token followed by a colon followed by an identifier
+-- Both the token and colon can be ignored only the result of identifier matters
+parseTokenColonIdentifier :: String -> Parser Char String
+parseTokenColonIdentifier t = token t *> pColon *> many anySymbol
+
+parseVersion :: Parser Char String
+parseVersion = parseTokenColonIdentifier "VERSION"
+
+parseProdId :: Parser Char String
+parseProdId = parseBeginVCal *> parseVersion *> parseTokenColonIdentifier "PRODID"
 
 -- Exercise 2
 readCalendar :: FilePath -> IO (Maybe Calendar)
-readCalendar = undefined
+readCalendar fp = do
+                  handle  <- openFile fp ReadMode
+                  _       <- hSetNewlineMode handle universalNewlineMode
+                  content <- hGetContents handle
+                  return undefined content
 
+type Token = String -- Convert Parser Type to Token ish
+
+readTest :: FilePath -> IO [Token]
+readTest fp = do
+              handle  <- openFile fp ReadMode
+              _       <- hSetNewlineMode handle universalNewlineMode
+              content <- hGetContents handle
+              return $ lines content
 
 -- Exercise 3
 -- DO NOT use a derived Show instance. Your printing style needs to be nicer than that :)
@@ -89,4 +178,3 @@ timeSpent = undefined
 -- Exercise 5
 ppMonth :: Year -> Month -> Calendar -> Doc
 ppMonth = undefined
-
