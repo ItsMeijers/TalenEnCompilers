ec4ff2d619729f9aaa88394c9209ad53faf8d10d
ThomasMeijers <thomasmeijers@live.nl>
2015-12-02 15:47:18 +0100

    Create parseVCalendar todo parseVEvent

--- 1Part2-iCal-Parser/ICalendar.hs
+++ 1Part2-iCal-Parser/ICalendar.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DisambiguateRecordFields, NamedFieldPuns, RecordWildCards #-}
+
 module ICalendar where
 
 import ParseLib.Abstract
@@ -10,6 +12,21 @@ data DateTime = DateTime { date :: Date
                          , time :: Time
                          , utc :: Bool }
     deriving (Eq, Ord)
+
+instance Show DateTime where
+  show = printDateTime
+    where printDateTime DateTime {..} = printDate date ++ "T" ++ printTime time ++ ['Z' | utc]
+            where printDate Date {..} = show' (unYear year)   4 ++
+                                        show' (unMonth month) 2 ++
+                                        show' (unDay day) 2
+                  printTime Time {..} = show' (unHour hour)     2 ++
+                                        show' (unMinute minute) 2 ++
+                                        show' (unSecond second) 2
+                  show' x             = fixLenght (show x)
+                  fixLenght xs n -- fixes the length of a value to its original form; year 1 gets fixed to 0001
+                    | length' >= n    = xs
+                    | otherwise       = replicate (n - length') '0' ++ xs
+                        where length' = length xs
 
 data Date = Date { year  :: Year
                  , month :: Month
@@ -103,20 +120,16 @@ main = do Just cal <- readCalendar "examples/rooster_infotc.ics"
 -- Exercise 1
 parseCalendar :: Parser Char Calendar
 parseCalendar = Calendar         <$>
-                (parseBeginVCal  *>
+                (parseBeginVCal  *> -- Begin and Version can be ignored for constructing Calendar
                 parseVersion     *>
                 parseProdId)     <*>
-                many parseVEvent <*
-                parseEndVCal
-
-parseBeginVEvent :: Parser Char String
-parseBeginVEvent = parseTokenColonToken "BEGIN" "VEVENT"
-
-parseEndVEvent :: Parser Char String
-parseEndVEvent = parseTokenColonToken "END" "VEVENT"
-
-parseUID :: Parser Char String
-parseUID = parseTokenColonIdentifier "UID"
+                many parseVEvent <* -- parses many events where the parser is specified for a single event
+                parseEndVCal        -- EndVCal can ignored for constructing Calendar
+      where
+        parseBeginVCal = parseTokenColonToken "BEGIN" "VCALENDAR" <* parseNewLine
+        parseVersion   = parseTokenColonIdentifier "VERSION"      <* parseNewLine
+        parseProdId    = parseTokenColonIdentifier "PRODID"       <* parseNewLine
+        parseEndVCal   = parseTokenColonToken "END" "VCALENDAR"   <* parseNewLine
 
 parseVEvent :: Parser Char VEvent
 parseVEvent = VEvent             <$>
@@ -128,29 +141,34 @@ parseVEvent = VEvent             <$>
               parseSummary       <*>
               parseLocation
 
+parseBeginVEvent :: Parser Char String
+parseBeginVEvent = parseTokenColonToken "BEGIN" "VEVENT" <* parseNewLine
+
+parseUID :: Parser Char String
+parseUID = parseTokenColonIdentifier "UID" <* parseNewLine
+
 parseDateTimeStamp :: Parser Char DateTime
-parseDateTimeStamp = parseTokenColonParser "DTSTAMP" parseDateTime
+parseDateTimeStamp = parseTokenColonParser "DTSTAMP" parseDateTime <* parseNewLine
 
 parseDateTimeStart :: Parser Char DateTime
-parseDateTimeStart = parseTokenColonParser "DTSTART" parseDateTime
+parseDateTimeStart = parseTokenColonParser "DTSTART" parseDateTime <* parseNewLine
 
 parseDateTimeEnd :: Parser Char DateTime
-parseDateTimeEnd = parseTokenColonParser "DTEND" parseDateTime
+parseDateTimeEnd = parseTokenColonParser "DTEND" parseDateTime <* parseNewLine
 
 parseDescription :: Parser Char (Maybe String)
-parseDescription = optional $ parseTokenColonIdentifier "DESCRIPTION"
+parseDescription = optional $ parseTokenColonIdentifier "DESCRIPTION" <* parseNewLine
+
+parseEndVEvent :: Parser Char String
+parseEndVEvent = parseTokenColonToken "END" "VEVENT" <* parseNewLine
 
 parseSummary :: Parser Char (Maybe String)
-parseSummary = undefined
+parseSummary = optional $ parseTokenColonIdentifier "SUMMARY" <* parseNewLine
 
 parseLocation :: Parser Char (Maybe String)
-parseLocation = undefined
+parseLocation = optional $ parseTokenColonIdentifier "LOCATION" <* parseNewLine
 
-parseBeginVCal :: Parser Char String
-parseBeginVCal = parseTokenColonToken "BEGIN" "VCALENDAR"
-
-parseEndVCal :: Parser Char String
-parseEndVCal = parseTokenColonToken "END" "VCALENDAR"
+-- Parser combinator abstractions :
 
 parseTokenColonToken :: String -> String -> Parser Char String
 parseTokenColonToken t t' = token t *> parseColon *> token t'
@@ -166,11 +184,8 @@ parseTokenColonIdentifier t = parseTokenColonParser t $ many anySymbol
 parseTokenColonParser :: String -> Parser Char a -> Parser Char a
 parseTokenColonParser t = (token t *> parseColon *>)
 
-parseVersion :: Parser Char String
-parseVersion = parseTokenColonIdentifier "VERSION"
-
-parseProdId :: Parser Char String
-parseProdId = parseTokenColonIdentifier "PRODID"
+parseNewLine :: Parser Char Char
+parseNewLine = symbol '\n'
 
 -- Exercise 2
 readCalendar :: FilePath -> IO (Maybe Calendar)
@@ -178,16 +193,15 @@ readCalendar fp = do
                   handle  <- openFile fp ReadMode
                   _       <- hSetNewlineMode handle universalNewlineMode
                   content <- hGetContents handle
-                  return undefined content
+                  return $ run parseCalendar content
 
 type Token = String -- Convert Parser Type to Token ish
 
-readTest :: FilePath -> IO [Token]
+readTest :: FilePath -> IO String
 readTest fp = do
               handle  <- openFile fp ReadMode
               _       <- hSetNewlineMode handle universalNewlineMode
-              content <- hGetContents handle
-              return $ lines content
+              hGetContents handle
 
 -- Exercise 3
 -- DO NOT use a derived Show instance. Your printing style needs to be nicer than that :)
